70e9087a32b398139c8ba9095a6add7a530864a3
yukai <kaiyu@caixin.com>
2017-09-06 17:30:37 +0800

    init

---
+++ app/src/main/java/com/paozhuanyinyu/freshmember/MainActivity.java
@@ -0,0 +1,15 @@
+package com.paozhuanyinyu.freshmember;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import paozhuanyinyu.com.freshmember.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/paozhuanyinyu/freshmember/MyService.java
@@ -0,0 +1,55 @@
+package com.paozhuanyinyu.freshmember;
+
+import android.accessibilityservice.AccessibilityService;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by Administrator on 2017/9/6.
+ */
+
+public class MyService  extends AccessibilityService{
+    private String currentActivityName;
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.d("MyService","onServiceConnected");
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+        Log.d("MyService","onAccessibilityEvent");
+        getCurrentActivityName(accessibilityEvent);
+    }
+
+    @Override
+    public void onInterrupt() {
+        Log.d("MyService","onInterrupt");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d("MyService","onUnbind");
+
+        return super.onUnbind(intent);
+    }
+    private void getCurrentActivityName(AccessibilityEvent event) {
+        if (event.getEventType() != AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            return;
+        }
+        try {
+            String pkgName = event.getPackageName().toString();
+            String className = event.getClassName().toString();
+            ComponentName componentName = new ComponentName(pkgName, className);
+            getPackageManager().getActivityInfo(componentName, 0);
+            currentActivityName = componentName.flattenToShortString();
+            Log.d("MyService", "cur=" + currentActivityName);
+        } catch (PackageManager.NameNotFoundException e) {
+            //只是窗口变化，并无activity调转
+            Log.d("MyService", "e=" + e.getLocalizedMessage());
+        }
+    }
+}
